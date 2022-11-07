edd61df080975f57be186bc7ce09e80c5e6d4c61
Deepthi M <79050917+deepthi1107@users.noreply.github.com>
2022-11-07 12:35:04 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello, world!"
+def print_num(x):
+    print(x)
+    
 
 
 if __name__ == "__main__":
-    app.run()
+    num=input()
+    print_num(num)
 
